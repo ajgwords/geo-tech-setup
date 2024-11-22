93355cedddedc3ee94afdc7f8a4efe7d14177ad6
Al Graham <llansteffan@hotmail.com>
2024-11-22 22:03:39 +0000

    python install update

--- scripts/04_python.sh
+++ scripts/04_python.sh
@@ -32,13 +32,35 @@ conda update -y conda
 echo "Installing mamba in the base environment..."
 conda install -n base -c conda-forge mamba -y
 
-# Step 3: Create the 'geo' environment with geopandas using mamba
+# Step 3: Add mamba to the .bashrc
+MAMBA_INIT_SCRIPT='if [ -f "/home/al/miniforge3/etc/profile.d/mamba.sh" ]; then
+    . "/home/al/miniforge3/etc/profile.d/mamba.sh"
+fi'
+BASHRC_FILE="$HOME/.bashrc"
+
+# Check if the code is already present in .bashrc
+if grep -Fxq "$MAMBA_INIT_SCRIPT" "$BASHRC_FILE"; then
+    echo "The mamba initialization script is already present in your .bashrc file."
+else
+    # Add the code to .bashrc
+    echo "Adding mamba initialization script to .bashrc file..."
+    echo -e "\n# Mamba initialization" >> "$BASHRC_FILE"
+    echo "$MAMBA_INIT_SCRIPT" >> "$BASHRC_FILE"
+    echo "Success"
+fi
+
+
+# Step 4: Create the 'geo' environment with geopandas using mamba
 if conda env list | grep -q "^$ENV_NAME\s"; then
     echo "The environment '$ENV_NAME' already exists."
 else
     echo "Creating environment '$ENV_NAME' with geopandas..."
     mamba create -n "$ENV_NAME" -c conda-forge geopandas -y
     echo "Environment '$ENV_NAME' created and geopandas installed."
+    echo "# Activate the '$ENV_NAME' environment on startup" >> "$bashrc_file"
+    echo "mamba activate geo" >> "$bashrc_file"
+    echo "Activation added successfully."
 fi
 
-echo "Setup complete. Use 'mamba activate $ENV_NAME' to activate the environment."
+source $HOME/.bashrc
+echo "Python setup complete. 
